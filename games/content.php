8369bb6c589f578e9ebaea58ed8977a052248693
SamAng219255 <SamAng219255@gmail.com>
2018-09-18 11:13:43 -0700

    updated the games description

--- games/content.php
+++ games/content.php
@@ -2,3 +2,8 @@
 <p>You likely won't survive a hostile encounter with a bear and if you do you will probably be finished off by the equally terrifying venomous snakes.</p>
 <br>
 <p><a href="./games/Alchahemistry/home/">Alchahemistry</a> is a very grindy multi-page game where you use fairly confusing alchemy to create strange objects with long descriptions which can be used to fight, protect yourself, and heal yourself.</p>
+<p>Some of the menus and the navigation bar come out small on mobile, but it is still playable. It stores your data in cookies.</p>
+<br>
+<p><a href="?target=.%2Fgames%2FDropBlock%2F">DropBlock</a> is a simple game I made in Unity.</p>
+<p>Blocks drop from the top of the screen and you are trying to maneuver them into groups of four, causing them to disappear. You lose when the blocks reach the top.</p>
+<p>It is not playable on mobile as it requires the arrow keys. In the future I may create a mobile app version.</p>
