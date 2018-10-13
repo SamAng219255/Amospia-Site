e6b844d946f7c726ef4a4e58e24483b279d6d629
SamAng219255 <SamAng219255@gmail.com>
2018-10-12 17:09:03 -0700

    updated

--- MCserver/map/tiling.js
+++ MCserver/map/tiling.js
@@ -42,7 +42,7 @@ function move(e) {
 		lastTar=[5,5];
 		$("#infoTxt")[0].innerHTML="";
 		clearTimeout(curTim);
-		setTimeout(draw,1000);
+		curTime=setTimeout(draw,3000);
 	}
 	if(e.keyCode==37) {
 		pos[0]--;
