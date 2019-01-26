341412b3541aa04089e9b3f9623128688ec489ce
SamAng219255 <SamAng219255@gmail.com>
2019-01-25 18:34:14 -0800

    deactivating some code to fix a problem I made by forgetting a 'WHERE'

--- MCserver/map/pinDescFix.php
+++ MCserver/map/pinDescFix.php
@@ -1,6 +1,6 @@
 <?php
 
-require 'db.php';
+/*require 'db.php';
 $pins=json_decode(file_get_contents('./pins.json'),true);
 var_dump($pins);
 $pinCount=count($pins);
@@ -8,6 +8,6 @@ for($i=0; $i<$pinCount; $i++) {
 	$sql="UPDATE `mcstuff`.`mappoints` SET `desc`='".mysqli_real_escape_string($conn,$pins[$i]['desc'])."' WHERE `id`='".$pins[$i]['id']."';";
 	echo "<span>".$sql."</span><br>";
 	mysqli_query($conn,$sql);
-}
+}*/
 
 ?>
