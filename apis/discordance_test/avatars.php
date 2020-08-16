9812ef1dc2378eebe19fc9fb86f6b7fc51a40fb7
SamAng219255 <SamAng219255@gmail.com>
2020-08-15 21:47:40 -0700

    fixed initial avatars api

--- apis/discordance_test/avatars.php
+++ apis/discordance_test/avatars.php
@@ -1,5 +1,5 @@
 <?php
-	$path=explode('/', $GET('path'));
+	$path=explode('/', $_GET['path']);
 	header('Content-Type: image/'.explode('.',$path[1])[1]);
-	readfile('img/avatars/'.$path[1]));
+	readfile('img/avatars/'.$path[1]);
 ?>
--- apis/discordance_test/index.php
+++ apis/discordance_test/index.php
@@ -1,13 +1,15 @@
 <?php
 	header('Content-Type: text/xml; charset=UTF-8');
 
-	$path=explode('/', $GET('path'));
+	$path=explode('/', $_GET['path']);
 	switch ($path[0]) {
 		case 'avatars':
 			require 'avatars.php';
 			break;
 		default:
-			echo '<Error><Code>NotImplemented</Code><Message>Endpoint has not been implemented.</Message><Details>Either I haven\'t gotten to it yet or I don\'t need it.</Details></Error>';
+			echo '<Error><Code>NotImplemented</Code><Message>Endpoint has not been implemented.</Message><Details>Either I haven\'t gotten to it yet or I don\'t need it. Path: ';
+			var_dump($path);
+			echo'</Details></Error>';
 			break;
 	}
 ?>
