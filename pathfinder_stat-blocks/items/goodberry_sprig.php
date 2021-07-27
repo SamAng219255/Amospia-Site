76fee224671a18d4446700dfe1d5caacad77b45d
SamAng219255 <SamAng219255@gmail.com>
2021-07-27 01:07:38 -0700

    Added another race and improved navigation

--- pathfinder_stat-blocks/items/goodberry_sprig.php
+++ pathfinder_stat-blocks/items/goodberry_sprig.php
@@ -10,7 +10,7 @@
 		"—",
 		"This sprig of what appears to be holly without the berries can be commanded once per day to grow goodberries as per the spell.",
 		false,
-		"bb/Requirements/bb Craft Wondrous Item, ii/goodberry/ii racial ability; bb/Cost/bb 360 gp"
+		"bb/Requirements/bb Craft Wondrous Item, ii/goodberry/ii; bb/Cost/bb 360 gp"
 	);
 	require '../pageEnd.php';
 ?>
--- pathfinder_stat-blocks/pageStart.php
+++ pathfinder_stat-blocks/pageStart.php
@@ -45,9 +45,14 @@
 								$tree_counts[$depth]=count($ptr['nodes']);
 								break;
 							
+							case 'flower':
 							case 'leaf':
 								$entry=$pages['entries'][$ptr['name']];
 								echo '<li><a href="'.$entry['directory'].$entry['file_name'].'"><p class="label">'.$ptr['display_name'].'</p></a></li>';
+								$tree_indices[$depth]++;
+								break;
+
+							case 'petal':
 								$tree_indices[$depth]++;
 								break;
 							
@@ -73,17 +78,77 @@
 	</div>
 	<div id="center">
 		<div id="top">
-			<?php
-				function retrieveAtDepth($arr,$path,$initial_depth=0) {
-					$depth=count($path);
-					$ptr=$arr;
-					for($i=$initial_depth; $i<$depth; $i++) {
-						$ptr=$ptr[$path[$i]];
+			<div id="top-nav">
+				<?php
+					function retrieveAtDepth($arr,$path,$initial_depth=0) {
+						$depth=count($path);
+						$ptr=$arr;
+						for($i=$initial_depth; $i<$depth; $i++) {
+							$ptr=$ptr[$path[$i]];
+						}
+						return $ptr;
 					}
-					return $ptr;
-				}
 
+					$pageId="";
+					$pageCount=count($pages['entries']);
+					$filename=pathinfo(debug_backtrace()[0]['file'])['basename'];
+					foreach($pages['entries'] as $id => $entry) {
+						if($entry['file_name']==$filename) {
+							$pageId=$id;
+						}
+					}
 
-			?>
+					$depth=0;
+					$tree_path=[$pages['sort_tree']];
+					$node_list=[];
+					$tree_index=0;
+					$tree_count=count($tree_path[$depth]);
+					$next_node=[];
+					$get_path=explode(',', $_GET['path']);
+
+					$sanity=100;
+					while($depth<count($tree_path)) {
+						if($sanity<1) {
+							echo '<p>Loop has gone insane after 100 iterations.</p>';
+							break;
+						}
+						if($tree_index<$tree_count) {
+							$ptr=$tree_path[$depth][$tree_index];
+							$match=$ptr['name']==(isset($_GET['path'])?$get_path:$pages['entries'][$pageId]['sort_path'])[$depth];
+							if($match) {
+								array_push($tree_path, $ptr['nodes']);
+								$next_node=$ptr;
+							}
+							if($tree_index==0) 
+								echo '<p class="top-row">';
+							else
+								echo ' | ';
+							if($ptr['type']=='branch') {
+								$search_path='';
+								foreach($node_list as $node) {
+									$search_path.=$node['name'].',';
+								}
+								$search_path.=$ptr['name'];
+								echo '<a href="'.$pages['origin'].'?path='.$search_path.'" class="top-label'.($match || $ptr['name'] == $pageId ? ' selected' : '').'">'.$ptr['display_name'].'</a>';
+							}
+							else {
+								$entry=$pages['entries'][$ptr['name']];
+								echo '<a href="'.$entry['directory'].$entry['file_name'].'" class="top-label'.($match || $ptr['name'] == $pageId ? ' selected' : '').'">'.$ptr['display_name'].'</a>';
+							}
+							$tree_index++;
+						}
+						else {
+							echo '</p><hr>';
+							$depth++;
+							if($depth>=count($tree_path))
+								break;
+							$tree_index=0;
+							$tree_count=count($tree_path[$depth]);
+							array_push($node_list, $next_node);
+						}
+						$sanity--;
+					}
+				?>
+			</div>
 		</div>
 		<div id="content">
