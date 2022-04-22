73ead0b15f3d58a1e11a53c1f56655647dbe865e
SamAng219255 <SamAng219255@gmail.com>
2022-04-22 12:08:15 -0700

    Filled in several tridents.

--- ttrpg_stat_blocks/pathfinder_1e/topics/amospia/items/spellbound_tridents.php
+++ ttrpg_stat_blocks/pathfinder_1e/topics/amospia/items/spellbound_tridents.php
@@ -29,48 +29,48 @@
 			'Complex Trident'
 		],
 		[
-			[
+			quick_array([
 				'Air',
 				'',
 				'',
 				'',
 				'Spells with the Air descriptor.'
-			],
-			[
+			]),
+			quick_array([
 				'Fire',
-				'',
-				'',
-				'',
+				'Launches a bead of flame that travels and explodes like ii/fireball/ii.',
+				'Launches a small controlled flame that swiftly travels up to long range to a location that you can see, potentially navigating around or through objects. Once it has arrived it erupts into a massive ball of flame like ii/fireball/ii except that area increases to a 40-foot-radius spread, the maximum damage increases to 20d6, a number of points of damage equal to your caster level ignore fire resistance and immunity, and it makes water and similar liquids in the area evaporate into steam and as such, it can be used underwater without issue.',
+				'Fires four controlled flames that function as ii/meteor swarm/ii except all the damage is fire damage, half of the damage dealt ignores fire resistance and immunity, and it destroys, melts, and evaporates objects as the simple version of the trident.',
 				'Spells with the Fire descriptor.'
-			],
-			[
+			]),
+			quick_array([
 				'Healing',
-				'',
-				'',
-				'',
+				'Cures 1d8 points of damage to a touched creature plus 1d8 per two caster levels over first, maximum 5d8. This is a positive energy effect and deals damage to undead instead of curing them. Undead may make a Will save for half damage.',
+				'Restores the life of a touched creature. When used you choose whether it behaves as ii/heal/ii or ii/breath of life/ii.',
+				'Restores a creature to full hit points and cures all negative levels, ability drain, and conditions cured by the ii/heal/ii spell. Aditionally, if cast on a creature that has died within the last minute, they are successfully healed and restored to life. As this is a positive energy effect, undead creatures are instead instantly slain.',
 				'Spells of the Healing sub-school.'
-			],
-			[
+			]),
+			quick_array([
 				'Luck',
 				'',
 				'',
 				'',
 				'Spells with the Luck descriptor.'
-			],
-			[
+			]),
+			quick_array([
 				'Water',
 				'',
 				'',
 				'',
 				'Spells with the Water descriptor.'
-			],
-			[
+			]),
+			quick_array([
 				'Force',
-				'Fires a single bolt of force. The bolt requires a ranged touch attack against your target and deals 1d6 points of damage plus 1d6 points of damage per two caster levels over first, maximum 5d6.',
-				'Fires one more powerful bolts of force that deal 5d6 points of force damage and throw creatures back like ii/battering blast/ii.',
+				'Fires a single bolt of force. The bolt requires a ranged touch attack against your target and deals 1d6 points of damage plus 1d6 points of damage per caster level over first, maximum 10d6.',
+				'Fires one or more powerful bolts of force that deal 5d6 points of force damage and throw creatures back like ii/battering blast/ii.',
 				'Fires one or more powerful homing bolts of force that deal 5d6 points of force damage plus 1d6 per two caster levels over 10th and throw creatures back like ii/battering blast/ii. These bolts also always hit their target like ii/magic missile/ii except they cannot be blocked by effects that block magic missile.',
 				'Spells with the Force descriptor.'
-			]
+			])
 		],
 		true,
 		true,
@@ -85,36 +85,36 @@
 			'Greater Simple Trident'
 		],
 		[
-			[
+			quick_array([
 				'Flight',
-				'',
-				'',
-				''
-			],
-			[
+				'Grants a touched creature the power to ii/fly/ii as per the spell.',
+				'Grants the wielder the power to fly as per the spell ii/overland flight/ii.',
+				'Grants the wielder the power to fly as per the spell ii/overland flight/ii except they recieve a bonus to their fly skill equal to their caster level, their speed is increased to 120 feet (or 80 feet if it wears medium or heavy armor, or if it carries a medium or heavy load) with a maneuverability of perfect. While under this effect, they do not need to make a check to hover or to move less than half their speed and remain flying.'
+			]),
+			quick_array([
 				'Teleportation',
-				'',
-				'',
-				''
-			],
-			[
+				'Fires a bolt of energy that travels a specified distance before teleporting the wielder to itself. This otherwise behaves as ii/dimension door/ii.',
+				'Allows the wielder to concentrate on a location to teleport themself and wouched objects and willing creatures as ii/teleport/ii.',
+				'Allows the wieler to open a doorway in space on front of them that otherwise functions as ii/gate/ii except it can only be used for planar travel function and it may be opened to another point on the same plane.'
+			]),
+			quick_array([
 				'Resurrection',
-				'',
-				'',
-				''
-			],
-			[
+				'Restores a dead creature to life as ii/reincarnate/ii.',
+				'Restores a dead creature to life as ii/raise dead/ii.',
+				'Restores a dead creature to life as ii/true resurrection/ii.'
+			]),
+			quick_array([
 				'Void',
-				'',
-				'',
-				''
-			],
-			[
+				'Damages an object within close range as per ii/break/ii.',
+				'Fires a ray of darkness and entropy from the end that behaves as ii/disintegrate/ii.',
+				'Conjures a sphere of pure darkness within long range that is identical to a ii/sphere of annihilation/ii except that control can be established by any creature holding the trident regardless of the distance and no other creature attempt to control the sphere. The wielder is also treated as bearing a ii/talisman of the sphere/ii for the purposes of controlling this sphere. Whenever the wielder make s a successful control check they may choose to cause the sphere to wink out instead. The sphere also winks out if it ever leaves the range of the trident or the trident is destroyed. Only one sphere may be created by each trident at a time.'
+			]),
+			quick_array([
 				'Control',
-				'',
-				'',
-				''
-			]
+				'Makes a nearby creature friendly as per ii/charm monster/ii.',
+				'Allows nearly complete control of a humanoid as per ii/dominate person/ii.',
+				'Allows complete control of a creature as per ii/dominate monster/ii except the subject never receives a new save and caries out any order, even obviously self-destructive ones. You can also issue complex telepathic commands even if you do not share a language. The subject also acts largely normal, unless given commands to the contrary, removing the decrease to the DC to determine the influence.'
+			])
 		],
 		true,
 		true,
@@ -129,36 +129,36 @@
 			'Greater Simple Trident'
 		],
 		[
-			[
+			quick_array([
 				'Motion',
 				'',
 				'',
 				''
-			],
-			[
+			]),
+			quick_array([
 				'Explosion',
 				'',
 				'',
 				''
-			],
-			[
+			]),
+			quick_array([
 				'Luminance',
-				'',
+				'The wielder cause either the end of the trident or a touched object to glow as ii/daylight/ii or they can dispell darkness spells of 3rd level or lower.',
 				'',
 				''
-			],
-			[
+			]),
+			quick_array([
 				'Satisfaction',
 				'',
 				'',
 				''
-			],
-			[
+			]),
+			quick_array([
 				'Storm',
 				'Summons a 5-foot-wide, 30-foot-long, vertical bolt of lightning that deals 1d6 points of electricity damage, plus 1d6 per 2 levels over first, within medium range as designated by aiming the trident. Any creature in the target square or in the path of the bolt is affected and may make a reflex save to take half damage.',
 				'Summons a bolt of lightning from the tip of the trident as per the spell ii/lightning bolt/ii.',
 				'Summons an arc of lightning from the tip of the trident that bounces to nearby targets as per the spell ii/chain lightning/ii.'
-			]
+			])
 		],
 		true,
 		true,
