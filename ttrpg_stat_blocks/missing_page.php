1fb015a16873d20ed3ad599af7a21edfff7fa03f
SamAng219255 <SamAng219255@gmail.com>
2022-06-14 12:08:27 -0700

    Updated missing page

--- ttrpg_stat_blocks/missing_page.php
+++ ttrpg_stat_blocks/missing_page.php
@@ -1,6 +1,6 @@
 <?php require 'pageStart.php'; ?>
 <title>D&D/Pathfinder</title>
 <p>I'm sorry. The page you are looking for doesn't exist... yet. It is quite possible that you just followed a link to apge I haven't made yet. While I try to avoid such links, they do occasionally appear.</p>
-<p>The name of the link may have also been misspelled.</p>
+<p>I may also have misspelled the link if I added it manually somewhere.</p>
 <p>If you followed a link on this site to this page, feel free to inform me of this issue at SamAng219255@gmail.com and I will remedy the issue.</p>
 <?php require 'pageEnd.php'; ?>
