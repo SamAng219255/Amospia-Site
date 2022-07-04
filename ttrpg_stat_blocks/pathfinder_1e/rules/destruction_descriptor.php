6dfa38ea5df9086fa5bfc0ac384c56ecbfe43b69
SamAng219255 <SamAng219255@gmail.com>
2022-07-04 09:13:58 -0700

    Clarified some text on the destruction descriptor

--- ttrpg_stat_blocks/pathfinder_1e/rules/destruction_descriptor.php
+++ ttrpg_stat_blocks/pathfinder_1e/rules/destruction_descriptor.php
@@ -15,7 +15,7 @@
 		quick_array([
 			'This is an additional descriptor added to better classify spells for aa/shadow|shadows/aa and their kin who have an affinity for destructive spells.',
 			'Spells with the destruction descriptor do not necessarily deal damage nor are most damaging spells destruction spells. Destruction spells are, instead, spells which directly cause the destruction of an object, similar to how spells of the creation sub-school directly create something regardless of what was created and goal of creating the thing.',
-			'As this descriptor is designed to be applied to existing canon spells retroactively, the following is a list of spells that gain the destruction descriptor. This list is incomplete and I will add to it as I determine additional spells.'
+			'As this descriptor is designed to be applied to existing canon spells retroactively, the following is a list of spells that gain the destruction descriptor. This list is incomplete and I will add to it as I determine additional spells. Spells marked with * are spells designed by me and can be found on site under aa/spells|spells/aa. These spells do have destruction listed as pne of their descriptors. Spells marked with ** are third-party spells not designed by me.'
 		])
 	);
 ?>
@@ -29,17 +29,17 @@
 	<li>eroding ray</li>
 	<li>greater break</li>
 	<li>greater shout</li>
-	<li>greater snap</li>
+	<li>greater snap*</li>
 	<li>implosion</li>
-	<li>lesser snap</li>
+	<li>lesser snap*</li>
 	<li>mage's disjunction</li>
 	<li>remove curse</li>
 	<li>resounding blow</li>
 	<li>rusting grasp</li>
 	<li>shatter</li>
 	<li>shout</li>
-	<li>snap</li>
+	<li>snap*</li>
 	<li>sympathetic vibration</li>
-	<li>thunderous snap</li>
+	<li>thunderous snap*</li>
 </ul>
 <?php require $startDir.'pageEnd.php'; ?>
