4d3a5f0c653b64cebac5f9881ad7c5bdeb26b3ec
SamAng219255 <SamAng219255@gmail.com>
2024-08-03 16:17:17 -0700

    Added icons to ttrpg stat blocks.

--- ttrpg_stat_blocks/pageStart.php
+++ ttrpg_stat_blocks/pageStart.php
@@ -76,6 +76,15 @@
 <html>
 <head>
 	<meta charset="utf-8">
+	<link rel="apple-touch-icon" sizes="180x180" href="/ttrpg_stat_blocks/img/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/ttrpg_stat_blocks/img/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/ttrpg_stat_blocks/img/favicon-16x16.png">
+	<link rel="manifest" href="/ttrpg_stat_blocks/img/site.webmanifest">
+	<link rel="mask-icon" href="/ttrpg_stat_blocks/img/safari-pinned-tab.svg" color="#5bbad5">
+	<link rel="shortcut icon" href="/ttrpg_stat_blocks/img/favicon.ico">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="msapplication-config" content="/ttrpg_stat_blocks/img/browserconfig.xml">
+	<meta name="theme-color" content="#ffffff">
 	<script src="/jquery.js"></script>
 	<script src="/ttrpg_stat_blocks/mobileDetect.js"></script>
 	<?php
