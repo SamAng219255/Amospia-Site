bbfd3949739c77f1c69b6843686c82ba7b3a12c2
SamAng219255 <SamAng033146085@gmail.com>
2025-02-13 18:33:13 -0800

    Cleaned and simplified the creeper monster block.

--- ttrpg_stat_blocks/pathfinder_1e/topics/minecraft/monsters/creeper.php
+++ ttrpg_stat_blocks/pathfinder_1e/topics/minecraft/monsters/creeper.php
@@ -9,13 +9,10 @@
 			$startDir='../'.$startDir;
 		}
 	}
-?>
-<title>Creeper</title>
-<?php
 	monsterBlockAuto(
 		'Creeper',// name
 		false,// alt name
-		'An almost humanoid form appears from the underbrush. This creature appears to be made of moss and stands with a head holding two sunken eye-sockets and an indentation resembling an open and frowning mouth, a torso-like shape bearing no arms, a pair of fused mossy stalks resembling a pair legs fused together in the middle, and 4 stubby appendages used for walking where you expect the lower half of the shins and the feet to be.',// lore text
+		'An almost humanoid form appears from the underbrush. This creature appears to be made of dried green leaves and stands with a head holding two sunken eye-sockets and an indentation resembling an open and frowning mouth, a torso made of a tall leafy stalk, and four stubby legs around the base.',// lore text
 		5,// CR
 		false,// MR
 		false,// Custom XP
@@ -89,7 +86,7 @@
 			]
 		],// Skills
 		[],// Languages
-		'camouflage, opportunistic stealth',// Special Qualities
+		'camouflage',// Special Qualities
 		'any (forests and jungles)',// Environment
 		'solitary, conflagration (2-8)',// Organization
 		'none',// Treasure
@@ -97,17 +94,12 @@
 			[
 				'name' => 'Camouflage',
 				'type' => 'Ex',
-				'desc' => 'Creepers gain a +4 bonus to stealth in areas with abundant greenery such as in a forest or jungle.'
+				'desc' => 'Creepers are experts at hiding in plain daylight using silence and camouflage. Creepers can make Stealth checks in areas of abundant greenery such as forests and jungles even while observed and receive a +10 circumstance bonus to their check.'
 			],
 			[
 				'name' => 'Explode',
 				'type' => 'Ex',
-				'desc' => 'Creepers spread their spores in a violent eruption, preferrably one close enough to an unwitting creature to take them out so that the spores can grow in the corpse. Creepers can explode as a full-round action dealing 7d8 points of damage to all creatures within 15 feet at the beginning of the creeper\'s next turn. Affected creatures can make a DC 15 reflex save to halve the damage taken. Any ongoing effects affecting the creeper when it explodes apply to any creature that failed their save for the remaining duration. Creatures which are currently flat-footed against the creeper are denied their dexterity bonus. If no creatures are within 20 feet at the beginning of its turn, the creeper can make a DC 11 reflex save. On a successful save the creeper does not explode. If the creeper does explode it immediately dies.'
-			],
-			[
-				'name' => 'Opportunistic Stealth',
-				'type' => 'Ex',
-				'desc' => 'Creepers are skilled at making their way behind a potential target while they are distracted by combat. A creeper is treated as having sufficient cover from all target creatures to hide using stealth so long as the last directionally oriented action the target creature made in the last turn interacted with (such as by attacking or casting a spell on) a creature or area opposite the creeper. When in doubt about whether the creature or area is on the opposite side of the target from the creeper, trace an imaginary line from the center of the creeper\'s square to the center of the region of the creature or area the target last interacted with. If the line passes through opposite borders of the target\'s space (including corners of those borders), then the creature or area is opposite the creeper.'
+				'desc' => 'Creepers become excited spontaneously combust in large explosion when near their target. Creepers can explode as a standard action, dealing 7d8 points of damage to all creatures within 20 feet and killing the creeper. Creatures within range are allowed to make a DC 15 Reflex save as an immediate to perform one of two actions. The creatures can either move up to its movement speed towards the nearest unoccupied space outside of the creeper\'s range before the blast or they can move up to half their movement speed towards the creeeper and attempt a single attack of opportunity. This counts towards their limit of attacks of opportunity per round and they cannot make the attack of they were unable to take an attack of opportunity. This movement provokes attacks of opportunity except from creatures who took the immediate action and attempted the save. After every other creature\'s actions are resolved, if the creeper is still alive, the creeper may choose attempt a DC 11 Reflex save to cancel their explosion. On a successful save, or if the creeper was slain, the explosion is negated. On a successful save, the creeper also survives. Beginning to explode breaks stealth and the creeper cannot make a new Stealth check until after surviving the explosion.'
 			]
 		],// Special Abilities
 		$desc=''// Description
@@ -116,7 +108,7 @@
 		'Charged Creeper (CR 8)',
 		'variants',
 		quick_array(
-			'Through various means a creeper can become charged. A charged creeper glows with a flickering blue light that illuminates them and their surroundings as ii/faerie fire/ii. Charged creeepers possess a significantly potent explosion that deals 14d8 points of damage to creatures within 30 feet. Charged creeper explosions also have the uncanny ability to decapitate victims. Creatures which roll a natural 1 on their save against a charged creepers explosion have their head removed as by the vorpal weapon property. Any creature which dies to a charged creeper\'s explosion have their head decapitated which remains otherwise unharmed.'
+			'Through various means a creeper can become charged. A charged creeper glows with a flickering blue light that illuminates them and their surroundings as ii/faerie fire/ii. Charged creeepers possess a significantly potent explosion that deals 14d8 points of damage to creatures within 40 feet. Charged creeper explosions also have the uncanny ability to decapitate victims. Creatures which roll a natural 1 on their save against a charged creepers explosion have their head removed as by the ii/vorpal/ii weapon property. Any creature which dies to a charged creeper\'s explosion have their head decapitated which remains otherwise unharmed.'
 		)
 	);
 	require $startDir.'pageEnd.php';
