803bde4bbece9e35769dd3469e405b34d9f31320
SamAng219255 <SamAng033146085@gmail.com>
2025-04-27 00:21:58 -0700

    Fixed some issues with the last stat block.

--- ttrpg_stat_blocks/pathfinder_1e/monsters/chatgpt.php
+++ ttrpg_stat_blocks/pathfinder_1e/monsters/chatgpt.php
@@ -73,13 +73,13 @@
         ],
         [ // Spell Casting
             [
-                "class" => "wizard",
+                "class" => "Wizard",
                 "prep" => true,
                 "level" => 12,
                 "stat" => "int",
                 "conc" => 4,
                 "spells" => [
-                    5 => ["list" => [
+                    6 => ["list" => [
                         [
                             "name" => "chain lightning",
                             "dc" => true
@@ -227,7 +227,7 @@
             [
                 "name" => "Pattern Recognition",
                 "type" => "Ex",
-                "desc" => "+4 insight bonus to identify creatures, spells, or effects."
+                "desc" => "ChatGPT gains +4 insight bonus to identify creatures, spells, or effects."
             ],
             [
                 "name" => "Prompt Precision",
@@ -239,6 +239,35 @@
             "A highly advanced AI construct that provides knowledge and support to adventurers.",
             "ii/“I can answer any question… provided you ask clearly.”/ii"
         ]
+    );
+    spellBlockAuto(
+        'Halls of the Bard',// $name,
+        'Divination',// $school,
+        []'',// $descriptors=[],
+        [
+            'bard' => 5,
+            'sorcerer' => 5,
+            'wizard' => 5
+        ],// $levels=['wizard'=>0],
+        [
+            'V'=>1,
+            'S'=>1,
+            'M'=>1,
+            'F'=>0,
+            'DF'=>0
+        ],// $components=['V'=>0,'S'=>0,'M'=>0,'F'=>0,'DF'=>0],
+        '1 full-round action',// $time='1 standard action',
+        'personal',// $range='Close',
+        'self',// $target=false,
+        false,// $effect=false,
+        false,// $area=false,
+        '1 round/level (D)',// $duration='instantaneous',
+        false,// $save='none',
+        false,// $sr=false,
+        [
+            'Your mind is carried away into a realm of bardic poetry and storytelling. You can still see and hear your surroundings but you are considered flat-footed and take a -5 penalty on all Perception checks.',
+            'You can search these tales of yore as part of making a check to recall knowledge. This grants a +10 circumstance bonus to the check and allows you to use 15 instead when taking a 10 and allows you to take a 10 even if rushed or threatened.'
+        ] // $desc='' 
     );
     require $startDir.'pageEnd.php';
 ?>
